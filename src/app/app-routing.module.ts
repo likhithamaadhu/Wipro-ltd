68a2af430e17e3521fd0bb7db3040602f2af0520
Likhitha <luckylikhita2@gmail.com>
2022-06-30 20:22:11 +0530

    first commit 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UtilityComponent } from './utility/utility.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { TeamComponent } from './team/team.component';
+
+//defining a route for navigation between components
+const routes: Routes = [
+  { path:'',component:HomeComponent},
+  { path:'about',component:AboutComponent},
+  { path:'calculator',component:UtilityComponent},
+  { path:'contact',component:ContactComponent},
+  { path:'team',component:TeamComponent},
+  { path:'employee',component:EmployeeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
